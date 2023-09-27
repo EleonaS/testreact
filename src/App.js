49dc3ee4cefe453a7d14cdd5af054da60ec67621
Elena Sysoyeva <evsysoyeva@gmail.com>
2023-09-27 20:12:53 +0300

    fix4

--- src/App.js
+++ src/App.js
@@ -4,16 +4,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          hello word
+          hello word hello word hello word
         </a>
       </header>
     </div>
